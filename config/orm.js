d7fe8154bf4fd1f57bd0207f920f7165a4bfe2f2
Aaron Lu <aaronnoh0409@gmail.com>
2021-02-23 05:41:37 -0500

    starting over

--- config/orm.js
+++ config/orm.js
@@ -10,24 +10,6 @@ var orm = {
 			cb(res);
 		});
 	},
-	insertOne: function(tableInput, columnName, burgerName, cb) {
-		var queryString = "INSERT INTO ?? (??) VALUES (?)";
-		connection.query(queryString, [tableInput, columnName, burgerName], function(err, res) {
-			if (err) {
-				throw err;
-			}
-			cb(res);
-		});
-	},
-	updateOne: function(tableInput, updateColumnName, updateRowVal, searchColumnName, searchRowVal, cb) {
-		var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
-		connection.query(queryString, [tableInput, updateColumnName, updateRowVal, searchColumnName, searchRowVal], function(err, res) {
-			if (err) {
-				throw err;
-			}
-			cb(res);
-		});
-	},
     
     deleteOne: function(tableInput, burgerId, cb) {
 		var queryString = "DELETE FROM ?? WHERE id = ?"
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,49 +2,6 @@ var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger.js");
 
-// Our GET request to grab database contents
-router.get("/", function(req, res) {
-	burger.selectAll(function(data) {
-		var hbsObject = {
-			burgers: data
-		};
-
-		res.render("index", hbsObject);
-	});
-});
-
-// Our POST request to add a burger to the database
-router.post("/", function(req, res) {
-	console.log(req.body.burger_name);
-	if(req.body.burger_name !== "") {
-		burger.insertOne(req.body.burger_name.trim(), function() {
-			res.redirect("/");
-		});
-	}
-});
-
-// Our PUT request to update a burger's status
-router.put("/:id", function(req, res) {
-	console.log(req.params.id);
-
-	burger.updateOne(req.params.id, function() {
-		res.redirect("/");
-	});
-})
-
-router.delete("/", (req, res) => {
-   var condition = "id= " + req.params.id;
-
-   console.log("condition", condition);
-
-   burger.deleteOne(condition, (result) => {
-     if (result.changedRows == 0) {
-       return res.status(404).end();
-     } else {
-       res.status(200).end();
-     }
-   });
-});
 
 
 module.exports = router;
