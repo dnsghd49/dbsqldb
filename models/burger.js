13eb7b6becaf6c671a9a2b54c82929539833bcca
Aaron Lu <aaronnoh0409@gmail.com>
2021-02-23 04:19:35 -0500

    messing around with the codes and at this point idk what im doing

--- models/burger.js
+++ models/burger.js
@@ -1,14 +1,31 @@
 var orm = require("../config/orm.js");
 
 // Call orm functions here
+var burger = {
+	selectAll: function(cb) {
+		orm.selectAll("burgers", function(res) {
+			cb(res);
+		});
+	},
+	insertOne: function(burgerName, cb) {
+		orm.insertOne("burgers", "burger_name", burgerName, function(res) {
+			cb(res);
+		});
+	},
+	updateOne: function(burgerId, cb) {
+		orm.updateOne("burgers", "devoured", 1, "id", burgerId, function(res) {
+			cb(res);
+		});
+	},
+
+	deleteOne: function(burgerId, cb) {
+		orm.deleteOne("burgers", "id", burgerId, function(res) {
+			cb(res);
+		});
+	}
 
 
-
-
-
-
-
-
+};
 
 
 module.exports = burger;
