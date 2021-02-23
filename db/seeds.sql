b60572259006435e475a688f632abf115c0f4582
Aaron Lu <aaronnoh0409@gmail.com>
2021-02-23 02:10:40 -0500

    setting up db

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,5 @@
-  
+USE burgers_db;
+
 INSERT INTO burgers (name) VALUES ('Cheeseburger');
 INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
 INSERT INTO burgers (name) VALUES ('Avacado Burger');
