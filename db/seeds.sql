586f32191404b0718a444f649d7c813e3ef3a9d8
Aaron Lu <aaronnoh0409@gmail.com>
2021-02-23 02:15:50 -0500

    setting up db

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
 USE burgers_db;
 
-INSERT INTO burgers (name) VALUES ('Cheeseburger');
-INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
-INSERT INTO burgers (name) VALUES ('Avacado Burger');
-INSERT INTO burgers (name) VALUES ('Pineapple Burger');
-INSERT INTO burgers (name) VALUES ('BBQ Onion Ring Burger');
-INSERT INTO burgers (name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Avacado Burger');
+INSERT INTO burgers (burger_name) VALUES ('Pineapple Burger');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Onion Ring Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
